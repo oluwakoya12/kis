c3d366c86b7a5a6d525694de5db02d94d9bc13de
oluwakoya12 <oluwakoyaisaaciyin@gmail.com>
2024-08-30 14:35:57 +0100

    updated spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 
 const Spinner = () => {
   return (
-    <div className="w-24 h-24 border-8 border-t-8 border-blue-500 border-opacity-25 border-t-blue-500 rounded-full animate-spin absolute top-[50%] left-[50%]"></div>
+    <div className="flex w-screen justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+    </div>
   );
 }
 
